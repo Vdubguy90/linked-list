1ba38b5e2f48c80ecf4b82306740c94858b9317e
Vdubguy90 <chorntvedt@ewu.edu>
2026-02-12 20:03:55 -0800

    last commit

---
+++ src/list/LinkedList.java
@@ -0,0 +1,36 @@
+package list;
+
+import org.junit.Test;
+
+public class LinkedList<T> implements List<T> {
+
+     private static class Node<T> {
+          public T data;        // The state of the inner class
+          public Node next;     // Pointer to next element
+     }
+
+     private int size;
+     
+     private Node head;       // Start of the list, 
+                              // has to point to the next element in the list
+
+     public LinkedList() {
+          this.size  = 0;
+     }
+     
+     @Override
+     public int size() {
+          return this.size;
+     }
+
+     @Override
+     public boolean isEmpty(){
+          return (size == 0);
+     }
+
+     @Override
+     public void add(T element){
+          size++;
+          
+     }
+}
--- src/test/UnitTests.java
+++ src/test/UnitTests.java
@@ -2,7 +2,7 @@
 package test;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
